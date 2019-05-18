7a3aaf4809891ecfd95fb8389df6dd82764f6296
Yunuen <yunuenm23@gmail.com>
2019-05-18 10:36:02 -0500

    fIRST COMMIT

---
+++ js/app.js
@@ -0,0 +1,11 @@
+'use strict'
+
+var entrar = 0;
+var altura = 101;
+if( altura > 100 || altura < 50){
+    entrar = 1;
+}
+else if(altura > 60 || altura < 40){
+    entrar = 2;
+}
+console.log(entrar);
